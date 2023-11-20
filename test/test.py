5cd69dc9692f02d012107f0a436bbc13a0431591
ghost9933 <mhatrenikhil36@gmail.com>
2023-11-20 13:47:32 -0600

    test

--- test/test.py
+++ test/test.py
@@ -1,12 +1,8 @@
-
 import re
-
-
 from google.cloud import dataproc_v1 as dataproc
 from google.cloud import storage
 
-
-def submit_job(project_id, region, cluster_name):
+def submit_job(project_id, region, cluster_name, job_file_path, output_bucket, output_blob_name):
     # Create the job client.
     job_client = dataproc.JobControllerClient(
         client_options={"api_endpoint": f"{region}-dataproc.googleapis.com:443"}
@@ -28,12 +24,19 @@ def submit_job(project_id, region, cluster_name):
     # allocated to the job. Use a regex to obtain the bucket and blob info.
     matches = re.match("gs://(.*?)/(.*)", response.driver_output_resource_uri)
 
-    output = (
-        storage.Client()
-        .get_bucket(matches.group(1))
-        .blob(f"{matches.group(2)}.000000000")
-        .download_as_bytes()
-        .decode("utf-8")
-    )
+    # Download the job output as bytes
+    output_bytes = storage.Client().get_bucket(matches.group(1)).blob(f"{matches.group(2)}.000000000").download_as_bytes()
 
-    print(f"Job finished successfully: {output}")
+    # Upload the job output to the specified output bucket and blob
+    storage.Client().bucket(output_bucket).blob(output_blob_name).upload_from_string(output_bytes.decode("utf-8"))
+
+    print(f"Job finished successfully. Output uploaded to gs://{output_bucket}/{output_blob_name}")
+
+if __name__ == "__main__":
+    project_id = "planar-depth-403402"
+    region = "us-south1"
+    cluster_name = "test"
+    job_file_path = 'gs://dataproc-examples/pyspark/hello-world/hello-world.py'
+    output_bucket = "your_output_bucket_name"
+    output_blob_name = "your_output_blob_name"
+    submit_job(project_id, region, cluster_name, job_file_path, output_bucket, output_blob_name)
