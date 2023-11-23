a8d1d681459866bb3bbb7cb4f94d024ea1ff4d66
ash-R2D2 <avinavashish008@gmail.com>
2023-11-23 10:04:24 -0600

    New Dataproc trigger views

--- dataprepBackend/projects/views.py
+++ dataprepBackend/projects/views.py
@@ -16,6 +16,51 @@ from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.dataframe.io import read_csv, to_csv
 from apache_beam.dataframe import expressions as exp
 
+class DataprocJobView(APIView):
+    def post(self, request, *args, **kwargs):
+        # Assuming the required parameters are passed in the POST request data
+        project_id = request.data.get('project_id')
+        region = request.data.get('region')
+        cluster_name = request.data.get('cluster_name')
+        job_file_path = request.data.get('job_file_path')
+        output_bucket = request.data.get('output_bucket')
+        output_blob_name = request.data.get('output_blob_name')
+        input_gcs_path = request.data.get('input_gcs_path')
+
+        if not all([project_id, region, cluster_name, job_file_path, output_bucket, output_blob_name, input_gcs_path]):
+            return Response({'error': 'Missing required parameters in the request.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            self.submit_job(project_id, region, cluster_name, job_file_path, output_bucket, output_blob_name, input_gcs_path)
+            return Response({'message': 'Job submitted successfully.'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': f'Error submitting job: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def submit_job(self, project_id, region, cluster_name, job_file_path, output_bucket, output_blob_name, input_gcs_path):
+        job_client = dataproc.JobControllerClient(
+            client_options={"api_endpoint": f"{region}-dataproc.googleapis.com:443"}
+        )
+
+        job = {
+            "placement": {"cluster_name": cluster_name},
+            "pyspark_job": {
+                "main_python_file_uri": job_file_path,
+                "args": ["--input", input_gcs_path]
+            },
+        }
+
+        operation = job_client.submit_job_as_operation(
+            request={"project_id": project_id, "region": region, "job": job}
+        )
+        response = operation.result()
+
+        matches = re.match("gs://(.*?)/(.*)", response.driver_output_resource_uri)
+        output_bytes = storage.Client().get_bucket(matches.group(1)).blob(f"{matches.group(2)}.000000000").download_as_bytes()
+
+        storage.Client().bucket(output_bucket).blob(output_blob_name).upload_from_string(output_bytes.decode("utf-8"))
+
+        print(f"Job finished successfully. Output uploaded to gs://{output_bucket}/{output_blob_name}")
+
 class dataProcApi(APIView):
     global dataproc_project_idproject_id, region, cluster_name
 
@@ -49,7 +94,9 @@ class dataProcApi(APIView):
                         }
                 operation = job_client.submit_job_as_operation(
                                 request={"project_id": dataproc_project_id, "region": region, "job": job})
+                print(" Data proc operation created..............")
                 response = operation.result()
+                print(response)
                 #gcloud dataproc jobs submit pyspark --cluster=dataprep-01-403222 gs://dataproc-examples/pyspark/hello-world/hello-world.py 
 
                 # Dataproc job output gets saved to the Google Cloud Storage bucket
@@ -63,6 +110,8 @@ class dataProcApi(APIView):
 
                 output_bucket = "gs://dataprep-bucket-001/Processed-Data"
                 output_blob_name = "processed_data_009.csv"
+
+                print(output_bucket, output_blob_name)
                 storage.Client().bucket(output_bucket).blob(output_blob_name).upload_from_string(output_bytes.decode("utf-8"))
 
                 print(f"Job finished successfully. Output uploaded to gs://{output_bucket}/{output_blob_name}")
