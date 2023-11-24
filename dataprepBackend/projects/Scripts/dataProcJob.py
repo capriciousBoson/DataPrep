dc869859ccdf367068519d568dd0383175736722
ash-R2D2 <avinavashish008@gmail.com>
2023-11-24 10:47:39 -0600

    added errro logs and dataproc job with multiple args

--- dataprepBackend/projects/Scripts/dataProcJob.py
+++ dataprepBackend/projects/Scripts/dataProcJob.py
@@ -6,33 +6,17 @@ from google.cloud import storage
 import json
 import sys
 
-def read_json_from_gcs(bucket_name, file_path):
-    # Create a storage client
-    storage_client = storage.Client()
-
-    # Get the bucket and blob
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(file_path)
-
-    # Download the JSON data
-    json_data = blob.download_as_text()
-
-    # Parse the JSON data into a Python dictionary
-    json_object = json.loads(json_data)
-
-    return json_object
-
 def mean_normalization(df, column_name):
     """
     Perform mean normalization on the specified column in the DataFrame.
     """
-    print("column anme is :: --- ",column_name)
     mean_value = df.agg(functions.mean(col(column_name))).collect()[0][0]
     std_dev_value = df.agg(functions.stddev(col(column_name))).collect()[0][0]
 
-    if std_dev_value ==0:
+    # Avoid division by zero
+    if std_dev_value == 0:
         return df
-    print("Calculated vlaues of mean =",mean_value," and std deviation is" ,std_dev_value," ...........................................................")
+
     normalized_column = ((col(column_name) - mean_value) / std_dev_value).alias(column_name + "_normalized")
     df_normalized = df.select("*", normalized_column)
 
@@ -44,41 +28,48 @@ def process_data(input_path, output_path, operations):
     and save the processed DataFrame to the specified output path.
     """
     spark = SparkSession.builder.appName("DataProcessing").getOrCreate()
+
     # Read CSV file from Google Cloud Storage
     df = spark.read.csv(input_path, header=True, inferSchema=True)
 
-    print(operations)
     # Perform specified operations
     for column_name, operation in operations.items():
         if operation == "mean_normalization":
-            print("staring mean normalization...................")
             df = mean_normalization(df, column_name)
-            print("finished mean normalization..............................")
         # Add more operations as needed
 
     # Write the processed DataFrame to Google Cloud Storage
     df.write.csv(output_path, header=True, mode="overwrite")
 
     spark.stop()
+
 if __name__ == "__main__":
     # Check if the correct number of command-line arguments are provided
-    """"
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <input_path> <output_path> <operations_dict>")
-        sys.exit(1)
-    """
+    #if len(sys.argv) != 4:
+    #    print("Usage: python script.py <input_path> <output_path> <operations_dict>")
+    #    sys.exit(1)
 
-    input_path = sys.argv[1]
-    output_path = "gs://dataprep-bucket-001/Processed-Data/processed_data_123467.csv"
-    operations_dict_str = {"reads":"mean_normalization"}
+    #input_path = 'gs://dataprep-bucket-001/Raw-Data/subset_dataset.csv'
+    print("sys.argv...........................",sys.argv,sys.argv[2],sys.argv[4],sys.argv[6])
+    input_path = sys.argv[2]
+    print("input_path................................",input_path, type(input_path))
+    
+    #operations_dict_str = sys.argv[3]
+    #output_path = "gs://dataprep-bucket-001/Processed-Data/processed_data_1230000.csv"
+    output_path = sys.argv[4]
+    #operations = {"reads":"mean_normalization"}
+
+    operations = sys.argv[6]
+    print("operations-----",operations,type(operations))
 
     try:
         # Convert the operations string to a dictionary
         #operations = eval(operations_dict_str)
 
         # Call the main processing function
-        process_data(input_path, output_path,operations_dict_str )
+        process_data(input_path, output_path, operations)
     except Exception as e:
         print(f"An error occurred: {e}")
         sys.exit(1)
+
 
--- dataprepBackend/projects/views.py
+++ dataprepBackend/projects/views.py
@@ -11,69 +11,37 @@ from google.cloud import dataproc_v1 as dataproc
 from google.cloud import storage
 
 
-import apache_beam as beam
-from apache_beam.runners.dataflow import DataflowRunner
-from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.dataframe.io import read_csv, to_csv
-from apache_beam.dataframe import expressions as exp
-
 class DataprocJobView(APIView):
     def post(self, request, *args, **kwargs):
-        # Assuming the required parameters are passed in the POST request data
+
         project_id = 'dataprep-01-403222'
         region = 'us-central1'
         cluster_name = 'dataprep-cluster-1'
 
-        username = request.data.get('username')
-        dataset_name = request.data.get("dataset_name")
-        config_data = request.data.get('config')
-        json_object = json.dumps([config_data], indent=2)
-
-        print(username, dataset_name, config_data)
-
-        print("Json object created...",json_object)
-
-        client = storage.Client.from_service_account_json(r'C:\Users\avina\Desktop\CLASS_NOTES\5333-Cloud_Computing\Project\DataPrep\dataprepBackend\confidential\dataprep-01-403222-8a783d347b8c.json')
-        bucket_name = 'dataprep-bucket-001'
-        filename = f'/Raw-Data/{username}/{dataset_name}.json'
-
-        bucket = client.get_bucket(bucket_name)
-        blob = bucket.blob(filename)
-
-        try:
-            blob.upload_from_string(json_object)
-            print("Json config file uploaded to GCS...", json_object)
-        except Exception as e:
-            print(f"Error uploading JSON to GCS: {str(e)}")
-
-        #blob.upload_from_string(json_object)
-        
-        print("Json config file uploaded to gcs...",json_object)
-        c='gs://dataprep-bucket-001/Raw-Data/ash101/subset_dataset.csv'
-        d='gs://dataprep-bucket-001/Raw-Data/ash101/subset_dataset.csv'
-
-
         job_file_path = 'gs://dataprep-jobs/Dataproc-Jobs/dataProcJob.py'
         #job_file_path = r"C:\Users\avina\Desktop\CLASS_NOTES\5333-Cloud_Computing\Project\DataPrep\dataprepBackend\projects\Scripts\dataProcJob.py"
         job_file_path2 = 'gs://dataproc-examples/pyspark/hello-world/hello-world.py'
-        output_bucket = "gs://dataprep-bucket-001/Processed-Data"
-        output_blob_name = "processed_data_999.csv"
+        output_bucket = "dataprep-bucket-001"
+        output_blob_name = "Processed-Data/processed_data_9999.csv"
+        input_gcs_path = 'gs://dataprep-bucket-001/Raw-Data/subset_dataset.csv'
+        output_csv_path = "gs://dataprep-bucket-001/Processed-Data/processed_data_1230000.csv"
         #input_gcs_path = 'gs://dataprep-bucket-001/Raw-Data/ash101/subset_dataset.csv'
-        input_gcs_path = f'gs://dataprep-bucket-001/Raw-Data/{username}/{dataset_name}.csv'
-        output_path = f"gs://dataprep-bucket-001/Processed-Data/{username}/{dataset_name}_processed_12346.csv"
+        #input_gcs_path = f'gs://dataprep-bucket-001/Raw-Data/{username}/{dataset_name}.csv'
+        #output_path = f"gs://dataprep-bucket-001/Processed-Data/{username}/{dataset_name}_processed_12346.csv"
 
-        print(input_gcs_path,'\n',output_path)
+        transformations ={"reads":"mean_normalization"}
+
 
         if not all([project_id, region, cluster_name, job_file_path, output_bucket, output_blob_name, input_gcs_path]):
             return Response({'error': 'Missing required parameters in the request.'}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            self.submit_job(project_id, region, cluster_name, job_file_path, output_bucket, output_blob_name, input_gcs_path, output_path)
+            self.submit_job(project_id, region, cluster_name, job_file_path, output_bucket, output_blob_name, input_gcs_path, output_csv_path, transformations)
             return Response({'message': 'Job submitted successfully.'}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': f'Error submitting job: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def submit_job(self, project_id, region, cluster_name, job_file_path, output_bucket, output_blob_name, input_gcs_path, output_path):
+    def submit_job(self, project_id, region, cluster_name, job_file_path, output_bucket, output_blob_name, input_gcs_path, output_csv_path, transformations):
         job_client = dataproc.JobControllerClient(
             client_options={"api_endpoint": f"{region}-dataproc.googleapis.com:443"}
         )
@@ -82,7 +50,7 @@ class DataprocJobView(APIView):
             "placement": {"cluster_name": cluster_name},
             "pyspark_job": {
                 "main_python_file_uri": job_file_path,
-                "args": ["--input", input_gcs_path]
+                "args": ["--input", input_gcs_path, "--output_path",output_csv_path, "--transformations", transformations]
             },
         }
 
@@ -98,66 +66,5 @@ class DataprocJobView(APIView):
 
         print(f"Job finished successfully. Output uploaded to gs://{output_bucket}/{output_blob_name}")
 
-        
 
-class dataflowJobsapi(APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = dfJobSerializer(data=request.data)
-        if serializer.is_valid():
-            metadata = serializer.validated_data
-            # Extract metadata values as needed
-            project_name =  metadata.get('project_name')
-            project_id =    metadata.get('project_id')
-            dataset_name =  metadata.get('dataset_name')
-            userame =       metadata.get('userame')
-
-            try:
-                # Your script execution logic here, using metadata values if needed
-                print(project_name, project_id ,dataset_name ,userame )
-
-                ###################################################################
-                PROJECT_ID='dataprep-01-403222'
-                JOB_NAME='dataprep-job-ash-009'
-                TEMP_DIR='gs://dataprep-bucket-001/temp'
-                STAGING_LOCATION ='gs://dataprep-bucket-001/staging'
-                REGION='us-south1'
-                SERVICE_ACCOUNT_EMAIL = "dataflow-gcs@dataprep-01-403222.iam.gserviceaccount.com"
-                KEY_FILE = 'DataFlow\dataprep-01-403222-12f89a955c05.json'
-                input_files = 'gs://dataprep-bucket-001/Raw-Data/subset_dataset.csv'
-
-                print("Params read !!!!!")
-                beam_options = PipelineOptions(
-                    project = PROJECT_ID,
-                    region = REGION,
-                    temp_location = TEMP_DIR,
-                    staging_location = STAGING_LOCATION,
-                    service_account_email=SERVICE_ACCOUNT_EMAIL,
-                    service_account_key=KEY_FILE,
-                    job_name = JOB_NAME,
-                    #num_workers=2
-                    )
-                print("beam options created!!!!")
-                column_name='reads'
-                p = beam.Pipeline(DataflowRunner(), options=beam_options)
-
-                df = p | read_csv(input_files, splittable=False)
-                print("csv read !!!!!")
-                #mean = df[column_name].mean()
-                #std = df[column_name].std()
-
-                #if std != 0:
-                    #df[column_name] = (df[column_name] - mean) / std
-
-                print("processing done!!!!")
-                df.to_csv('gs://dataprep-bucket-001/Processed-Data/processed_data_07.csv')
-
-                p.run().wait_until_finish()
-
-                ####################################################################
-                print("Job done !!!!!!!")
-                return Response({'success': True, 'result': "here should be result!!!!!"})
-            except Exception as e:
-                return Response({'success': False, 'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        else:
-            return Response({'success': False, 'message': 'Invalid metadata'}, status=status.HTTP_400_BAD_REQUEST)
 
